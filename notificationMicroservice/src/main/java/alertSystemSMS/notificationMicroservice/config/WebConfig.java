f4013774cb035cf7dfaee4d4333d84d958613555
gorggogi <diligcharlene@gmail.com>
2025-12-14 23:43:52 +0800

    Update WebConfig.java

--- notificationMicroservice/src/main/java/alertSystemSMS/notificationMicroservice/config/WebConfig.java
+++ notificationMicroservice/src/main/java/alertSystemSMS/notificationMicroservice/config/WebConfig.java
@@ -17,7 +17,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(apiKeyAuthInterceptor)
-                .addPathPatterns("/api/**")
-                .excludePathPatterns("/api/v1/send/direct");
+                .addPathPatterns("/api/**");
     }
 }
